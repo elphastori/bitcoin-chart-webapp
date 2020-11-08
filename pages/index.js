bb18040882659eaaac57724a934c18cac500126b
Elphas <elphas@house.me>
2020-11-08 22:15:38 +0200

    Fetch transactions from backend

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,9 @@ export default function Home() {
   const [hoverLoc, setHoverLoc] = useState(null);
   const [activePoint, setActivePoint] = useState(null);
 
+  const [fetchingTransactions, setFetchingTransactions] = useState(false);
+  const [transactions, setTransactions] = useState([]);
+
   const handleChartHover = (hoverLoc, activePoint) => {
     setHoverLoc(hoverLoc);
     setActivePoint(activePoint);
@@ -21,9 +24,9 @@ export default function Home() {
   useEffect(() => {
     const getData = async () => {
       const url = 'https://api.coindesk.com/v1/bpi/historical/close.json';
-      const r = await fetch(url);
 
       try {
+        const r = await fetch(url);
         const bitcoinData = await r.json();
 
         const sortedData = [];
@@ -46,11 +49,30 @@ export default function Home() {
     getData();
   }, []);
 
-  const transactions = [
-    { d: '2020-10-22', isBuy: true, amount: 12 },
-    { d: '2020-11-04', isBuy: false, amount: -24 },
-    { d: '2020-10-12', isBuy: true , amount: 36}
-];
+  const fetchTransactions = async () => {
+    setFetchingTransactions(true);
+    const url = 'https://api.luno.com/api/1/accounts/id/transactions?min_row=1&max_row=40';
+
+    try {
+      const r = await fetch(url);
+      const transData = await r.json();
+
+      console.log("transData", transData);
+
+      // const transactions = [
+      //   { d: '2020-10-22', isBuy: true, amount: 12 },
+      //   { d: '2020-11-04', isBuy: false, amount: -24 },
+      //   { d: '2020-10-12', isBuy: true, amount: 36 }
+      // ];
+
+      setTransactions(transData);
+      setFetchingTransactions(false);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+
 
   return (
     <div className={styles.container}>
@@ -70,17 +92,22 @@ export default function Home() {
           Interactive Bitcoin Chart
         </h1>
 
-        {!fetchingData ? <InfoBox data={data} /> : null }
+        {!fetchingData ? <InfoBox data={data} /> : null}
 
         <div className={styles.popup}>
           {hoverLoc && <ToolTip hoverLoc={hoverLoc} activePoint={activePoint} transactions={transactions} />}
         </div>
 
         {!fetchingData && <LineChart data={data} onChartHover={(a, b) => handleChartHover(a, b)} transactions={transactions} />}
+
+        <input type="button" value="Import Luno transactions" />
+        API Key ID: <input type="text" />
+        Secret: <input type="text" />
+
       </main>
 
       <footer className={styles.footer}>
-          Powered by&nbsp;
+        Powered by&nbsp;
           <a href="http://www.coindesk.com/price/" target="_blank" rel="noopener noreferrer">CoinDesk</a>
       </footer>
     </div>
